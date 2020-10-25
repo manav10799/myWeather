489019b43b8c6b187519cce0bf97c24f90e98772
Manav <43153689+manav10799@users.noreply.github.com>
2020-10-25 14:49:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ $(() => {
         let temperature;
         const unit="metric";
         console.log(city)
-        let res = $.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a900735ccdce7afd6b32191ff8d5f604&units=${unit}`, (data) => {
+        let res = $.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid={Your Api Id}&units=${unit}`, (data) => {
             temperature = data.main.temp
             console.log(temperature)
             $('.box_1').append(
